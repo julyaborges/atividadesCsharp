9efd8f09990e0af53d5492b1f8a290954003bdbd
Ana Julya <borgesanajulya@gmail.com>
2026-03-27 11:56:54 -0300

    Segundo commit

--- Atividade3.cs
+++ Atividade3.cs
@@ -1,23 +1,20 @@
 using System;
 
-// A rodada inicia em 1
-int rodadas = 1;
+/*
+└── IniciarJogo()
+       ├── Jogadores()
+       └── IniciarRodadas(1)   ← RECURSÃO
+              ├── ExecutarRodada()
+              ├── MostrarPlacar()
+              └── IniciarRodadas(rodada + 1)
+       
+Final:
+ └── Mostrar vencedor
+*/
 
-Console.WriteLine("Bem-vindo - Jogar Dados!");
-
-//Insira o nome do primeiro jogador
-Console.WriteLine("Insira o nome do primeiro jogador:");
-string jogador1 = Console.ReadLine();
-
-//Insira o nome do segundo jogador
-Console.WriteLine("Insira o nome do segundo jogador:");
-string jogador2 = Console.ReadLine();
-
-// Enquanto as rodadas forem: 1 2 3
-while(rodadas <= 3)
+public class Jogadores
 {
-    rodadas++;
-
-
-
+    public string nome;
+    public int pontuacao;
 }
+
